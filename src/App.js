510678cfb5136194b1772f7a835fb1e14a038161
Anton <79325492+anton001-web@users.noreply.github.com>
2022-06-29 18:09:27 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {galleryData} from "./components/galleryData";
+import {useState} from "react";
+import prev from './images/prev.png'
+import next from './images/next.png'
+import {logDOM} from "@testing-library/react";
 
 export default App;
+
+function App() {
+    const sliderModal = React.createRef()
+    const [current, setCurrent] = useState(0)
+    const slides = galleryData.length
+
+    const toggleModalSlider = (e) => {
+        if(e.target.dataset.setslide) {
+            const slide = parseInt(e.target.dataset.slide)
+            setCurrent(slide)
+            if(sliderModal.current.classList.contains('active')) {
+                sliderModal.current.classList.remove('active')
+            } else {
+                sliderModal.current.classList.add('active')
+            }
+        } else if(e.target.dataset.control) {
+            sliderModal.current.classList.remove('active')
+        }
+    }
+
+    const prevSlide = () => setCurrent(current === 0 ? slides - 1 : current - 1)
+    const nextSlide = () => setCurrent(current === slides - 1 ? 0 : current + 1)
+
+    return (
+        <>
+            <div className='gallery-block'>
+                <div className='slider' ref={sliderModal} data-control onClick={toggleModalSlider}>
+                    <div className='slide'>
+                        <div className='slider-btns'>
+                            <div className='btn-block' onClick={prevSlide}>
+                                <img className='slider-btn btn-prev' src={prev}/>
+                            </div>
+                            <div className='btn-block' onClick={nextSlide}>
+                                <img className='slider-btn btn-next' src={next}/>
+                            </div>
+                        </div>
+                        {
+                            galleryData.map((img, ind) => (
+                                ind === current && (
+                                    <img className='slider-img' src={img.src}/>
+                                )
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className='gallery-list'>
+                    {
+                        galleryData && galleryData.map((item, index) => (
+                            <div className='card' key={index}>
+                                <img  src={item.src} data-slide={index} data-setslide className='img' onClick={toggleModalSlider}/>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ src/components/galleryData.js
@@ -0,0 +1,6 @@
+export const galleryData = [
+    {src: 'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg'},
+    {src: 'https://ichef.bbci.co.uk/news/999/cpsprodpb/79F2/production/_123381213_06.jpg'},
+    {src: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'},
+    {src: 'https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg'},
+]
